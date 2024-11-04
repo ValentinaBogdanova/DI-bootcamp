4871418032735fcb9bcb2c10bbe8645348ab9aef
Valentina <bogdanova.valya@gmail.com>
2024-11-04 22:38:00 +0200

    добавила дейли

---
+++ Week1/Day 2/daily_ch2.py
@@ -0,0 +1,26 @@
+#CHALLENGE 1
+#Ask the user for a number and a length
+#Create a program that prints a list of multiples of the number until the list length reaches length.
+
+# user_input=input("Write two numbers, where first one is number and second is a length, put a space between")
+# user_num, user_lenght = map(int, user_input.split())
+# final_multi = [] 
+# for i in range(1, user_lenght + 1):
+#     final_multi.append(user_num * i)
+# print(f'number:{user_num}, lenght:{user_lenght}, {final_multi}')
+
+
+#CHALLENGE 2
+#Write a program that asks a string to the user, and display a new string with any duplicate consecutive letters removed.
+# user_word=input("Write a word like you scream it out. example: stooooooop")
+# user_word1=user_word.lower()
+# final_word=[]
+# for i in range(len(user_word1)):
+#     if i == 0 or user_word1[i] != user_word1[i-1]:
+#         final_word.append(user_word1[i])
+# word = ''.join(final_word)
+# print(word)
+
+
+
+
